c9415d76fcfbd778bee0c0eb4c753cb32ea18eb7
Elrond/ <marius.ciucam@gmail.com>
2023-01-06 17:27:46 +0200

    FIX: Exit on non zero

--- scripts/shard-meta-tests.sh
+++ scripts/shard-meta-tests.sh
@@ -5,15 +5,21 @@ startTest(){
 
   screen -L -A -m -d -S firehosenode ./integration-test.sh $1
 
-  sleep 80
+  sleep 90
 
   echo "finish starting firehosenode"
   echo "starting integration tests for $1"
   if [[ "$1" == "shard" ]]
   then
     ./scripts
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
   else
     ./scripts --check-meta
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
   fi
 
   echo "finished integration tests for $1"
